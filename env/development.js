bb6c3a69640aee41adb2d68278c6c9a4da7a1d22
Nagaraj <santnagaraj@deloitte.com>
2016-11-27 18:10:40 +0530

    Dev config changes

--- env/development.js
+++ env/development.js
@@ -5,12 +5,12 @@
 module.exports = {
   config: {
     host: '127.0.0.1',
-    port: '8080',
+    port: '8000',
     database: 'dev',
     user: 'user',
     password: 'user',
     charset: 'utf8'
-  },    
+  },
   loggerFileLocation: 'logs/app.log',
   mongodb: {
     url: 'mongodb://localhost:27017/daytotable'
